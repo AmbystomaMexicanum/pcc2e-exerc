19264fc5ad721f73f945cab415701b9d4f39de35
A. mexicanum <2528067149@qq.com>
2021-07-22 18:28:07 +0800

    Update and rename e_3_2.py to e_3_2_gh.py
    Replace original friends names with other names. Add descriptive comments at the beginning

--- src/ch_03/e_3_2.py
+++
@@ -1,5 +0,0 @@
-names = ['chen qiyuan', 'xu nuo', 'liang zhengyan']
-
-print(f'Good afternoon, {names[0].title()}! How are you today?')
-print(f'Good afternoon, {names[1].title()}! How are you today?')
-print(f'Good afternoon, {names[2].title()}! How are you today?')
---
+++ src/ch_03/e_3_2_gh.py
@@ -0,0 +1,9 @@
+# An edited, specifically-for-GitHub version.
+# Original names of my friends are replaced with
+# an alias, or just the name of someone else.
+
+names = ['origin chen', 'bill gates', 'carl linnaeus']
+
+print(f'Good afternoon, {names[0].title()}! How are you today?')
+print(f'Good afternoon, {names[1].title()}! How are you today?')
+print(f'Good afternoon, {names[2].title()}! How are you today?')
