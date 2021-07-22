612da2acfad736fb366430e3e07a40c78867db92
A. mexicanum <2528067149@qq.com>
2021-07-22 17:54:57 +0800

    Rename two_three.py to e_2_3.py

---
+++ e_2_3.py
@@ -0,0 +1,2 @@
+name = 'Cynthia'
+print(f'Hello {name}, would you like to give me some encouragement on fighting against depression?')
--- two_three.py
+++
@@ -1,2 +0,0 @@
-name = 'Cynthia'
-print(f'Hello {name}, would you like to give me some encouragement on fighting against depression?')
