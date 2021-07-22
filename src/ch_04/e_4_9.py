d38ccef9615cb7e4b62c7a8a8d5ed8ef9e77b56e
A. mexicanum <2528067149@qq.com>
2021-07-22 22:49:46 +0800

    Update e_4_9.py
    Minor edits in the comment

--- src/ch_04/e_4_9.py
+++ src/ch_04/e_4_9.py
@@ -1,3 +1,3 @@
-# I know Python has ** operand, but as ex-C++ programmer I habitually use * a couple of times
+# I know Python has `**` operand, but as an ex-C++ programmer I habitually use simple `*`s
 cubs = [i*i*i for i in range(1, 11)]
 print(cubs)
