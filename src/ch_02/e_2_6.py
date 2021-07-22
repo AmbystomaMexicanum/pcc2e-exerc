daeed40afd523d7f57fde320b6ae40b9806786c3
A. mexicanum <2528067149@qq.com>
2021-07-22 18:13:08 +0800

    Update e_2_6.py
    A minor English-specific semantical edit. `adage` > `saying`

--- src/ch_02/e_2_6.py
+++ src/ch_02/e_2_6.py
@@ -1,5 +1,5 @@
 famous_person = 'xu fengcan'
-adage = "It doesn't matter whether a soldier is male or female."
+saying = "It doesn't matter whether a soldier is male or female."
 
-message = f'{famous_person.title()} once said, "{adage}"'
+message = f'{famous_person.title()} once said, "{saying}"'
 print(message)
