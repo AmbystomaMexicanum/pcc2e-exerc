ab3c16873ec07ccf1344d523f8c8c309acd2b108
A. mexicanum <2528067149@qq.com>
2021-07-22 18:25:21 +0800

    Update e_3_1_gh.py
    Add descriptive comments at the beginning of the file

--- src/ch_03/e_3_1_gh.py
+++ src/ch_03/e_3_1_gh.py
@@ -1,3 +1,7 @@
+# Edited, specifically-for-GitHub version.
+# Original names of my friends are replaced with an
+# alias, or just a name of someone else.
+
 names = ['origin chen', 'bill gates', 'carl linnaeus']
 
 print('My friends:\n')
